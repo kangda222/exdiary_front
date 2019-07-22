bd8c5d0192a60cf46081fda933cde443d438b736
DaeunKang <jeangrey@xhiftcorp.com>
2019-07-22 16:14:52 +0900

    calender test

--- screens/CalenderScreen/index.js
+++ screens/CalenderScreen/index.js
@@ -2,9 +2,31 @@ import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { Calendar } from "react-native-calendars";
 
+const vacation = { key: "vacation", color: "red" };
+const massage = { key: "massage", color: "blue" };
+const workout = { key: "workout", color: "green" };
+
 const CalendarScreen = () => (
   <View style={styles.container}>
-    <Calendar />
+    <Calendar
+      monthFormat={"yyyy MM"}
+      markedDates={{
+        "2019-07-16": { marked: true, dotColor: "red" },
+        "2019-07-17": {
+          dots: [vacation, massage, workout],
+          selected: true
+        }
+        //'2019-06-18': {marked: true, dotColor: 'red', activeOpacity: 0},
+        // '2012-05-19': {disabled: true, disableTouchEvent: true}
+      }}
+      markingType={"multi-dot"}
+      onDayPress={day => {
+        console.log("selected day", day);
+      }}
+      onMonthChange={month => {
+        console.log("month changed", month);
+      }}
+    />
   </View>
 );
 
