69761c69ac7f97878494377b44164fc17dca3de8
DaeunKang <jeangrey@xhiftcorp.com>
2019-07-26 18:19:27 +0900

    mypage 화면

--- components/Modal/action.js
+++ components/Modal/action.js
@@ -1,13 +1,6 @@
 import React, { Component } from "react";
-import Modal from "react-native-modal";
-import { Dimensions, StatusBar, TouchableOpacity, Alert } from "react-native";
-import styled from "styled-components/native";
-import { Ionicons } from "@expo/vector-icons";
-import ProfileModal from "../ProfileModal";
-import PasswordModal from "../PasswordModal";
-
-const { width, height } = Dimensions.get("window");
-const STATUSBAR_HEIGHT = StatusBar.currentHeight;
+import { Alert } from "react-native";
+import ModalScreen from "./screen";
 
 class Action extends Component {
   constructor(props) {
@@ -23,45 +16,15 @@ class Action extends Component {
   render() {
     //console.log(width, height);
     return (
-      <Modal
-        isVisible={this.props.isModalVisible}
-        onBackdropPress={() => this.props.toggleModal()}
-        onSwipeComplete={() => this.props.toggleModal()}
-        swipeDirection="right"
-      >
-        <Container>
-          <Profile>
-            <Ionicons name="ios-contact" size={50} />
-            <Username>
-              {this.props.profile && this.props.profile.email}
-            </Username>
-          </Profile>
-          <TouchableOpacity onPressOut={() => this._toggleProfileModal()}>
-            <Menu>프로필 설정</Menu>
-            <ProfileModal
-              isVisibleProfile={this.state.isVisibleProfile}
-              toggleProfileModal={this._toggleProfileModal}
-              profile={this.props.profile}
-            />
-          </TouchableOpacity>
-          <TouchableOpacity onPressOut={() => this._togglePasswordModal()}>
-            <Menu>비밀번호 변경</Menu>
-            <PasswordModal
-              isVisiblePassword={this.state.isVisiblePassword}
-              toggleModal={this._togglePasswordModal}
-            />
-          </TouchableOpacity>
-          <TouchableOpacity>
-            <Menu>좋아요</Menu>
-          </TouchableOpacity>
-          <TouchableOpacity onPressOut={() => this._secession()}>
-            <Menu>회원탈퇴</Menu>
-          </TouchableOpacity>
-          <TouchableOpacity onPressOut={() => this._Alert()}>
-            <Menu>로그아웃</Menu>
-          </TouchableOpacity>
-        </Container>
-      </Modal>
+      <ModalScreen
+        {...this.state}
+        {...this.props}
+        toggleProfileModal={this._toggleProfileModal}
+        togglePasswordModal={this._togglePasswordModal}
+        logout={this._logout}
+        secession={this._secession}
+        Alert={this._Alert}
+      />
     );
   }
 
@@ -92,25 +55,5 @@ class Action extends Component {
     this.props.secession();
   };
 }
-
-const Container = styled.View`
-  background-color: #fff;
-  height: ${height};
-  width: ${width * (2 / 3)};
-  margin-left: ${width * (1 / 3)};
-`;
-
-const Profile = styled.View`
-  flex-direction: row;
-  margin: ${STATUSBAR_HEIGHT}px 2px 2px;
-`;
-
-const Username = styled.Text`
-  font-weight: 500;
-  font-size: 25;
-  margin-left: 10;
-`;
-
-const Menu = styled.Text``;
 
 export default Action;
